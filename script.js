e6de946b672d8a9588062d7f80f833316a61ef03
siriberi sudhakar <siriberisudhakar@gmail.com>
2024-01-02 22:13:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//document.write("Hello World")
+//alert("Hello World")
+//console.log("Hello World")
+
+document.write("Hello World");
+//alert("Hello World");
+console.log("Hello World");
+console.log({name: "sudhakar"});
+
+
+
+// multi line comments use control+/ select all lines what your commenting
+
+
+
+let firstname = "sirberi";
+
+console.log(firstname);
+
+let lastname, zip, state;
+
+lastname = "sudhakar";
+zip = 534305;
+state = "Ap";
+console.log(lastname , zip , state);
+
+
+
+
+
+
+// using let var and const 
+
+let something = "nothing";
+    something = "anything"
+
+console.log(something);
+
+
+var val = 1252;
+    val = 52
+console.log(val);
+
+
+const month = "december"
+    //error month = "navamber"
+    //month
+// month = "may"  can't do this 
+console.log(month);
